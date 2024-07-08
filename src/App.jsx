c37be6432b0b7d70bba6944b1f180ca30e555095
enquire07 <145021641+enquire07@users.noreply.github.com>
2024-07-08 22:47:30 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ const App = () => {
             <Route path="/" element={<Home />} />
             <Route path="/movies" element={<Movies />} />
             <Route path="/movies/:movieId/*" element={<MovieDetails />} />
-            {/* <Route path="/movies/:movieId/cast/*" element={<Cast />} /> */}
+            <Route path="/movies/:movieId/cast/*" element={<Cast />} />
             <Route path="*" element={<Navigate to="/" />} />
           </Routes>
         </Suspense>
